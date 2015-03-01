29408722da9c9fc1123d4484f7551c304c8d7942
Romain Goffe <romain.goffe@gmail.com>
2015-03-01 20:12:39 +0100

    [registration] add global open/close dates

--- globals.php
+++ globals.php
@@ -3,4 +3,22 @@
 $GLOBALS['registration-fee'] = 15.0;
 $GLOBALS['available-places'] = 100;
 
+$hour = 0;
+$minute = 0;
+$second = 0;
+$month = 4;
+$day = 11;
+$year = 2015;
+
+$GLOBALS['registration-open-date'] = mktime(hour, minute, second, month, day, year);
+
+$hour = 0;
+$minute = 0;
+$second = 0;
+$month = 5;
+$day = 31;
+$year = 2015;
+
+$GLOBALS['registration-close-date'] = mktime(hour, minute, second, month, day, year);
+
 ?>
