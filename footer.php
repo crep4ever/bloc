78dc26aecb14ea166f6a24de84b9b6106a7afdf0
Romain Goffe <romain.goffe@gmail.com>
2015-03-01 18:52:09 +0100

    [html] add partners + copyright info in footer

--- footer.php
+++ footer.php
@@ -1,2 +1,15 @@
+
+    <!-- Footer -->
+    <footer id="footer">
+      <ul class="icons">
+	<li><a href="http://www.ffme.fr"><img src="images/partners/ffme.png" title="FFME" alt="FFME" /></a></li>
+	<li><a href="http://www.ffcam.fr"><img src="images/partners/ffcam.png" title="FFCAM" alt="FFCAM" /></a></li>
+      </ul>
+
+      <span class="copyright">
+	&copy; Copyright <a href="http://fontaine-en-montagne.ffcam.fr/">CAF Fontaine</a>. All rights reserved. Design by <a href="http://www.html5webtemplates.co.uk">Responsive Web Templates</a>
+      </span>
+    </footer>
+
   </body>
 </html>
