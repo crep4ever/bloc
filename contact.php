e23371ff6eb81632818a13dc2eb1a5dae3afbd8f
Romain Goffe <romain.goffe@gmail.com>
2015-03-19 01:56:55 +0100

    [html] restore contact page

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php include("header.php"); ?>
+
+<section id="main" class="wrapper style1">
+ <header class="major">
+ <h2>Contactez-nous !</h2>
+ <p>Le meilleur moyen d'avoir une réponse à vos questions</p>
+ </header>
+ <div class="container">
+
+ <!-- Content -->
+ <section id="content">
+
+ <!-- Text -->
+ <section class="feature fa-envelope-o">
+   <p>
+     Pour des informations complémentaires ou toute question sur le
+     déroulement et les inscriptions, merci de nous contacter à l'adresse suivante :<br />
+     <a href="mailto:opendebloc.grenoble@gmail.com">opendebloc.grenoble@gmail.com</a></p>
+ </section>
+
+ </div>
+</section>
+
+<?php include("footer.php"); ?>
