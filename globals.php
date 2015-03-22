c305b6d702dd91a9ecc65126de43e4ef624e3471
Romain Goffe <romain.goffe@gmail.com>
2015-03-22 21:54:12 +0100

    [php] add arrival dates to globals

--- globals.php
+++ globals.php
@@ -22,4 +22,12 @@ $day = 31; $month = 5;  $year = 2015;
 $GLOBALS['registration-close-date'] = mktime($hour, $minute, $second, $month, $day, $year);
 $GLOBALS['registration-close-date-str'] = strftime('%A %e %B %G', $GLOBALS['registration-close-date']);
 
+$hour = 11; $minute = 0; $second = 0;
+$day = 7;   $month = 6;  $year = 2015;
+$GLOBALS['poussin-benjamin-arrival'] = mktime($hour, $minute, $second, $month, $day, $year);
+
+$hour = 8; $minute = 30; $second = 0;
+$day = 7;  $month = 6;   $year = 2015;
+$GLOBALS['minime-cadet-arrival'] = mktime($hour, $minute, $second, $month, $day, $year);
+
 ?>
