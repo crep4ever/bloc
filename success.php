196808a867efc965851455f68fda4a6ee71a65a0
Romain Goffe <romain.goffe@gmail.com>
2015-03-22 17:40:41 +0100

    [php] update paypal return pages + send email confirmation

--- success.php
+++ success.php
@@ -1,4 +1,35 @@
-<?php include("header.php"); ?>
+<?php 
+session_start();
+
+function sendConfirmation($firstName, $lastName, $mail)
+{
+  require 'globals.php';
+
+  $event = $GLOBALS['event-date-str'];
+  $subject = "[Open de Bloc] Confirmation d'inscription";
+  $body = "Bonjour,\r\n\r\n"
+    . "Vous avez réalisé l'inscription du participant $firstName $lastName pour l'open de bloc de Grenoble 2015.\r\n\r\n"
+    . "Rendez-vous $event à Espace Vertical 3 en possession :\r\n"
+    . " - d'une pièce d'identité du participant\r\n"
+    . " - de sa licence\r\n"
+    . " - de l'autorisation parentale écrite de participation à la compétition\r\n\r\n"
+    . "Merci et à bientôt !\r\n\r\n"
+    . "Le CAF Fontaine";
+
+  $body = wordwrap($body, 80, "\r\n");
+
+  $headers = 'From: Open de bloc Grenoble <opendebloc.grenoble@gmail.com>\r\n'
+    . 'Reply-To: opendebloc.grenoble@gmail.com\r\n';
+
+  mail($mail, $subject, $body, $headers);
+}
+
+sendConfirmation($_SESSION['firstname'],
+		 $_SESSION['lastname'],
+		 $_SESSION['mail']);
+
+include("header.php");
+?>
 
 <section id="main" class="wrapper style1">
 
@@ -9,16 +40,16 @@
     <section id="content">
 
       <p>
-	L'inscription du participant <b><?php echo $firstname ?> <?php echo $lastname ?></b>
+	L'inscription du participant <b><?php echo $_SESSION['firstname'] ?> <?php echo $_SESSION['lastname'] ?></b>
 	a été réalisée avec succès.
       </p>
 
       <p>
-	Une confirmation du paiement vous a été envoyée par email à l'adresse <b><?php echo $mail ?></b>.
+	Une confirmation du paiement vous a été envoyée par email à l'adresse <b><?php echo $_SESSION['mail'] ?></b>.
       </p>
 
       <p>
-	Nous vous donnons rendez-vous le <b>dimanche 7 juin</b>
+	Nous vous donnons rendez-vous le <b><?php echo $GLOBALS['event-date-str'] ?></b>
 	à <a href="access.php">Espace Vertical 3</a>. N'oubliez pas
 	de vous munir :
 	<ul>
