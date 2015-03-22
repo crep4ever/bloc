b5db4e6d5cac13b8795c8e4fd5cdce27bea6e89d
Romain Goffe <romain.goffe@gmail.com>
2015-03-22 18:47:01 +0100

    [content] typo

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 	<li><a href="http://www.ffcam.fr"><img src="images/logos/ffcam.jpg" title="FFCAM" alt="FFCAM" /></a></li>
 	<li><a href="http://ville-fontaine.fr"><img src="images/logos/fontaine.jpg" title="Ville de Fontaine" alt="Ville de Fontaine" /></a></li>
 	<li><a href="http://www.auvieuxcampeur.fr"><img src="images/logos/vieux-campeur.jpg" title="Au vieux campeur" alt="Au vieux campeur" /></a></li>
-	<li><a href="http://www.adidas.fr"><img src="images/logos/adidas.jpg" title="Au vieux campeur" alt="Au vieux campeur" /></a></li>
+	<li><a href="http://www.adidas.fr"><img src="images/logos/adidas.jpg" title="Adidas" alt="Adidas" /></a></li>
       </ul>
 
       <span class="copyright">
