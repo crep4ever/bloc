11fa6a1dbd932fcdc5c7d9b619213c5e2aa047e3
Romain Goffe <romain.goffe@gmail.com>
2015-03-26 19:25:51 +0100

    [content] rework media page

--- media.php
+++ media.php
@@ -5,71 +5,183 @@
     <h2>Retours sur l'événement</h2>
   </header>
   <div class="container">
-    <div class="row 150%">
-      <div class="4u 12u$(2)">
-	
-	<!-- Sidebar -->
-	<section id="sidebar">
-	  <section>
-	    <footer>
-	      <ul>
-		<li><a href="#quotes">Témoignages</a></li>
-		<li><a href="#photo">Sélection photos</a></li>
-		<li><a href="#video">Sélection vidéos</a></li>
-	      </ul>
-	    </footer>
-	  </section>
-	  <hr />
-	</section>
 
+    <nav class="subnav">
+      <ul>
+	<li><a href="#quotes">Témoignages</a></li>
+	<li><a href="#photos">Photos</a></li>
+	<li><a href="#video">Vidéos</a></li>
+      </ul>
+    </nav>
+
+    <!-- Content -->
+    <section id="content">
+
+      <!--    **************** QUOTES ******************    -->
+      <div id="quotes">
+	<hr />
+	<h3>Témoignages</h3>
+	
+	<h4>Participant 1</h4>
+	<blockquote>Fringilla nisl. Donec accumsan interdum nisi,
+	  quis tincidunt felis sagittis eget tempus
+	  euismod. Vestibulum ante ipsum primis in faucibus
+	  vestibulum. Blandit adipiscing eu felis iaculis volutpat
+	  ac adipiscing accumsan faucibus. Vestibulum ante ipsum
+	  primis in faucibus lorem ipsum dolor sit amet nullam
+	  adipiscing eu felis.</blockquote>
+
+	<h4>Participant 2</h4>
+	<blockquote>Fringilla nisl. Donec accumsan interdum nisi,
+	  quis tincidunt felis sagittis eget tempus
+	  euismod. Vestibulum ante ipsum primis in faucibus
+	  vestibulum. Blandit adipiscing eu felis iaculis volutpat
+	  ac adipiscing accumsan faucibus. Vestibulum ante ipsum
+	  primis in faucibus lorem ipsum dolor sit amet nullam
+	  adipiscing eu felis.</blockquote>
       </div>
-      <div class="8u 12u$(2) important(2)">
-	
-	<!-- Content -->
-	<section id="content">
 
-	  <h3 id="quotes">Témoignages</h3>
-	  
-	  <h4>Participant 1</h4>
-	  <blockquote>Fringilla nisl. Donec accumsan interdum nisi,
-	    quis tincidunt felis sagittis eget tempus
-	    euismod. Vestibulum ante ipsum primis in faucibus
-	    vestibulum. Blandit adipiscing eu felis iaculis volutpat
-	    ac adipiscing accumsan faucibus. Vestibulum ante ipsum
-	    primis in faucibus lorem ipsum dolor sit amet nullam
-	    adipiscing eu felis.</blockquote>
+      <!--    **************** PHOTOS ******************    -->
+      <script type="text/javascript">
+      $(document).ready(function() {
+          $('.fancybox').fancybox({
+            margin : 80,
+            maxHeight : 800
+          });
+      });
+      </script>
 
-	  <h4>Participant 2</h4>
-	  <blockquote>Fringilla nisl. Donec accumsan interdum nisi,
-	    quis tincidunt felis sagittis eget tempus
-	    euismod. Vestibulum ante ipsum primis in faucibus
-	    vestibulum. Blandit adipiscing eu felis iaculis volutpat
-	    ac adipiscing accumsan faucibus. Vestibulum ante ipsum
-	    primis in faucibus lorem ipsum dolor sit amet nullam
-	    adipiscing eu felis.</blockquote>
+      <div id="photos">
+	<hr />
+	<h3>Sélection photos</h3>
 
-	  <h3 id="photos">Sélection photos</h3>
+	<div class="box alt">
+	  <div class="row 50% uniform">
 
-	  <div class="box alt">
-	    <div class="row 50% uniform">
-	      <div class="4u 6u(3)"><span class="image fit"><img src="images/pic01.jpg" alt="" /></span></div>
-	      <div class="4u 6u(3)"><span class="image fit"><img src="images/pic02.jpg" alt="" /></span></div>
-	      <div class="4u 6u(3)"><span class="image fit"><img src="images/pic03.jpg" alt="" /></span></div>
-	      <div class="4u 6u(3)"><span class="image fit"><img src="images/pic02.jpg" alt="" /></span></div>
-	      <div class="4u 6u(3)"><span class="image fit"><img src="images/pic03.jpg" alt="" /></span></div>
-	      <div class="4u 6u(3)"><span class="image fit"><img src="images/pic01.jpg" alt="" /></span></div>
-	      <div class="4u 6u(3)"><span class="image fit"><img src="images/pic03.jpg" alt="" /></span></div>
-	      <div class="4u 6u(3)"><span class="image fit"><img src="images/pic01.jpg" alt="" /></span></div>
-	      <div class="4u 6u(3)"><span class="image fit"><img src="images/pic02.jpg" alt="" /></span></div>
-	    </div>
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_1.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_1.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_2.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_2.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_3.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_3.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_4.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_4.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_5.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_5.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_6.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_6.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_7.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_7.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_8.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_8.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_9.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_9.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_10.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_10.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_11.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_11.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_12.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_12.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_13.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_13.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_14.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_14.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_15.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_15.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_16.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_16.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_17.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_17.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_18.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_18.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+
 	  </div>
-	  
-
-	  <h3 id="movies">Sélection vidéos</h3>
-
-	</section>
+	</div>
       </div>
-    </div>
+
+      <!--    **************** VIDEOS ******************    -->
+      <div id="video">
+	<hr />
+	<h3>Sélection vidéos</h3>
+
+	<div style="text-align: center;">
+	  <iframe frameborder="0" width="800" height="450" src="//www.dailymotion.com/embed/video/x10tmc5" allowfullscreen></iframe><br /><a href="http://www.dailymotion.com/video/x10tmc5_teaser-open-de-bloc-jeune-de-grenoble-2013-hd_sport" target="_blank">Teaser Open de Bloc Jeune de Grenoble 2013 HD</a> <i>by <a href="http://www.dailymotion.com/CMEV" target="_blank">CMEV</a></i>
+	</div>
+
+      </div>
+    </section>
   </div>
 </section>
 
