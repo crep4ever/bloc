e43489636910479968e2cf8d6e7fd48c9b73b658
NyanVivi <legviv@gmail.com>
2015-07-10 16:17:05 +0200

    [ajout video youtube]

--- media.php
+++ media.php
@@ -1,4 +1,113 @@
-<?php include("header.php"); ?>
+<!DOCTYPE HTML>
+
+<html lang="fr">
+
+  <head>
+    <title>Open de bloc 2015</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="Open de bloc de Grenoble 2015" />
+    <meta name="keywords" content="escalade, compétition, bloc, grenoble, caf fontaine" />
+    <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/compressed/index.js"></script>
+    <script src="js/skel.min.js"></script>
+    <script src="js/skel-layers.min.js"></script>
+    <script src="js/init.js"></script>
+    <noscript>
+      <link rel="stylesheet" href="css/skel.css" />
+      <link rel="stylesheet" href="css/style.css" />
+      <link rel="stylesheet" href="css/style-xlarge.css" />
+    </noscript>
+    <!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+    <!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="57x57" href="images/icons/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="images/icons/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="images/icons/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="images/icons/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="images/icons/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="images/icons/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="images/icons/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="images/icons/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="images/icons/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="images/icons/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="images/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="images/icons/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images/icons/favicon-16x16.png">
+<link rel="manifest" href="images/icons/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="images/icons/ms-icon-144x144.png">
+
+
+<!-- Open graph -->
+<meta property="og:title" content="Open de bloc Grenoble 2015" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="http://www.openblocgrenoble.fr" />
+<meta property="og:image" content="http://www.openblocgrenoble.fr/images/open-bloc-2015.jpg" />
+
+<!-- Add fancyBox main JS and CSS files -->
+<script type="text/javascript" src="js/compressed/fancybox.js"></script>
+<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css?v=2.1.5" media="screen" />
+
+<script type="text/javascript">
+$(document).ready(function() {
+
+    // Set custom style, close if clicked, change title type and overlay color
+    $(".fancybox-effects-c").fancybox({
+	closeClick : true,
+
+	openEffect : 'none',
+
+	helpers : {
+	    title : {
+		type : 'inside'
+	    },
+	    overlay : {
+		css : {
+		    'background' : 'rgba(238,238,238,0.85)'
+		}
+	    }
+	}
+    });
+});
+</script>
+
+  </head>
+  <body class="landing">
+   <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-5736177-6', 'auto');
+  ga('send', 'pageview');
+
+</script>
+    <!-- Header -->
+    <header id="header" class="alt skel-layers-fixed">
+      <h1>
+	<a href="index.php">Open de bloc Grenoble 2015</a>
+	<span>&nbsp;&nbsp;par le&nbsp;&nbsp;</span>
+	<a href="http://fontaine-en-montagne.ffcam.fr" target="_blank">CAF Fontaine
+	<span>
+	  <img src="images/logos/caf-fontaine.png" alt="CAF Fontaine" height="40" />
+	</span>
+	</a>
+      </h1>
+      <nav id="nav">
+	<ul>
+	  <li><a href="index.php">Accueil</a></li>
+	  <li><a href="program.php">Déroulement</a></li>
+	  <li><a href="registration.php">Inscription</a></li>
+	  <li><a href="benevoles.php">Bénévoles</a></li>
+	  <li><a href="media.php">Media</a></li>
+	  <li><a href="results.php">Résultats</a></li>
+	  <li><a href="contact.php">Contact</a></li>
+	</ul>
+      </nav>
+    </header>
 
 <section id="main" class="wrapper style1">
   <header class="major">
@@ -6,40 +115,18 @@
   </header>
   <div class="container">
 
+
     <nav class="subnav">
       <ul>
-	<li><a href="#quotes">Témoignages</a></li>
-	<li><a href="#photos">Photos</a></li>
-	<li><a href="#videos">Vidéos</a></li>
+	<li><a href="#Video">Vidéo</a></li>
+	<li><a href="#photos">Sélection</a></li>
+	<li><a href="#podiums">Podiums</a></li>
+	<li><a href="#album">Album</a></li>
       </ul>
     </nav>
 
     <!-- Content -->
     <section id="content">
-
-      <!--    **************** QUOTES ******************    -->
-      <div id="quotes">
-	<hr />
-	<h3>Témoignages</h3>
-	
-	<h4>Participant 1</h4>
-	<blockquote>Fringilla nisl. Donec accumsan interdum nisi,
-	  quis tincidunt felis sagittis eget tempus
-	  euismod. Vestibulum ante ipsum primis in faucibus
-	  vestibulum. Blandit adipiscing eu felis iaculis volutpat
-	  ac adipiscing accumsan faucibus. Vestibulum ante ipsum
-	  primis in faucibus lorem ipsum dolor sit amet nullam
-	  adipiscing eu felis.</blockquote>
-
-	<h4>Participant 2</h4>
-	<blockquote>Fringilla nisl. Donec accumsan interdum nisi,
-	  quis tincidunt felis sagittis eget tempus
-	  euismod. Vestibulum ante ipsum primis in faucibus
-	  vestibulum. Blandit adipiscing eu felis iaculis volutpat
-	  ac adipiscing accumsan faucibus. Vestibulum ante ipsum
-	  primis in faucibus lorem ipsum dolor sit amet nullam
-	  adipiscing eu felis.</blockquote>
-      </div>
 
       <!--    **************** PHOTOS ******************    -->
       <script type="text/javascript">
@@ -51,9 +138,19 @@
       });
       </script>
 
+
+      <div id="video">
+	<hr />
+	<h3>Vidéo</h3>
+
+<iframe width="640" height="360" src="https://www.youtube.com/embed/hsW-9uYXcBM?rel=0" frameborder="0" allowfullscreen></iframe>s
+
+      </div>
+
+
       <div id="photos">
 	<hr />
-	<h3>Sélection photos</h3>
+	<h3>Sélection</h3>
 
 	<div class="box alt">
 	  <div class="row 50% uniform">
@@ -166,12 +263,161 @@
 		</a>
 	    </span></div>
 
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_19.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_19.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_20.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_20.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_21.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_21.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_22.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_22.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_23.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_23.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_24.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_24.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
 
 	  </div>
 	</div>
       </div>
 
+      <!--    **************** PODIUMS ******************    -->
+
+      <div id="podiums">
+	<hr />
+	<h3>Podiums</h3>
+
+	<div class="box alt">
+	  <div class="row 50% uniform">
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_1.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_1.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_2.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_2.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_3.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_3.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+	    
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_4.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_4.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_5.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_5.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_6.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_6.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_7.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_7.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_8.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_8.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_9.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_9.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_10.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_10.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_11.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_11.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_12.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_12.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_13.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_13.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_14.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_14.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	    <div class="4u 6u(3)"><span class="image fit">
+		<a class="fancybox" data-fancybox-group="gallery" href="media/open_bloc_grenoble_podiums_15.jpg">
+		  <img src="media/thumbnails/open_bloc_grenoble_podiums_15.jpg" alt="Open de Bloc Grenoble 2015" />
+		</a>
+	    </span></div>
+
+	  </div>
+	</div>
+      </div>
+      
+      <div id="album">
+	<hr />
+	<h3>Album souvenir</h3>
+
+	Vous pouvez consulter et télécharger une plus large sélection
+	de photos sur <a href="https://goo.gl/photos/Ur4FD8qx9gXzPG3Q8">l'album photo de l'Open de Bloc de Grenoble 2015</a>.
+
+      </div>
+      
       <!--    **************** VIDEOS ******************    -->
+      <!--
       <div id="videos">
 	<hr />
 	<h3>Sélection vidéos</h3>
@@ -187,8 +433,67 @@
 	</div>
 
       </div>
+      -->
     </section>
   </div>
 </section>
 
-<?php include("footer.php"); ?>
+
+    <!-- Footer -->
+    <footer id="footer">
+      <ul class="icons">
+	<li><a href="http://www.ffme.fr" target="_blank"><img src="images/logos/ffme.png" title="FFME" alt="FFME" /></a></li>
+	<li><a href="http://www.ffcam.fr" target="_blank"><img src="images/logos/ffcam.jpg" title="FFCAM" alt="FFCAM" /></a></li>
+	<li><a href="http://ville-fontaine.fr" target="_blank"><img src="images/logos/fontaine.jpg" title="Ville de Fontaine" alt="Ville de Fontaine" /></a></li>
+	<li><a href="http://www.auvieuxcampeur.fr" target="_blank"><img src="images/logos/vieux-campeur.jpg" title="Au vieux campeur" alt="Au vieux campeur" /></a></li>
+	<li><a href="http://www.adidas.fr" target="_blank"><img src="images/logos/adidas.jpg" title="Adidas" alt="Adidas" /></a></li>
+	<li><a href="http://www.beal-planet.com/" target="_blank"><img src="images/logos/beal.png" title="Beal" alt="Beal" /></a></li>
+	<li><a href="http://www.jegrimpe.com/" target="_blank"><img src="images/logos/jegrimpe.png" title="JeGrimpe.com" alt="JeGrimpe.com" /></a></li>
+	<li><a href="http://www.lechaussemontagne.fr/7-chaussons-d-escalade" target="_blank"><img src="images/logos/canin.png" title="Canin" alt="Canin" /></a></li>
+	<li><a href="http://www.andrea-boldrini.com/" target="_blank"><img src="images/logos/andrea-boldrini.jpg" title="André Boldrini" alt="Andréa Boldrini" /></a></li>
+	<li><a href="http://www.expe.fr/fr" target="_blank"><img src="images/logos/expe.png" title="Expé" alt="Expé" /></a></li>
+	<li><a href="http://www.grimper.com/" target="_blank"><img src="images/logos/grimper.png" title="Grimper" alt="Grimper" /></a></li>
+	<li><a href="http://entre-prises.fr/" target="_blank"><img src="images/logos/entre-prises.png" title="Entre-Prises" alt="Entre-Prises" /></a></li>
+	<li><a href="http://www.atelierrocabilo.com/" target="_blank"><img src="images/logos/atelier-rocabilo.gif" title="Atelier Rocabilo" alt="Atelier Rocabilo" /></a></li>
+	<li><a href="http://www.prises-escalade-osmose.com/" target="_blank"><img src="images/logos/osmose.png" title="Osmose" alt="Osmose" /></a></li>
+	<li><a href="http://www.ledauphine.com/" target="_blank"><img src="images/logos/DL.png" title="Dauphiné Libéré" alt="Dauphiné Libéré" /></a></li>
+
+
+
+      </ul>
+
+      <span class="copyright">
+	&copy; Copyright <a href="http://fontaine-en-montagne.ffcam.fr/">CAF Fontaine</a>. All rights reserved. Design by <a href="http://www.html5webtemplates.co.uk">Responsive Web Templates</a>.
+      </span>
+    </footer>
+
+   <script type="application/ld+json">
+{
+  "@context": "http://schema.org/",
+  "@type": "SportsEvent",
+  "name": "Open de bloc Grenoble 2015",
+  "image": "http://openblocgrenoble.fr/images/open-bloc-2015.jpg",
+  "organizer": {
+    "name": "CAF Fontaine",
+    "image": "http://openblocgrenoble.fr/images/logos/caf-fontaine.png",
+    "url": "http://fontaine-en-montagne.ffcam.fr/"
+  },
+  "typicalAgeRange": "10-17",
+  "description": "3ème édition de l'Open de bloc de Grenoble",
+  "startDate": "2015-06-07T08:30-0400",
+  "location": {
+    "@type": "Place",
+    "name": "Espace vertical 3",
+    "address": "22 rue Victor Lastella, 38000 Grenoble, France",
+    "sameAs": "http://openblocgrenoble.fr/access.php"
+  },
+  "offers": {
+    "@type": "Offer",
+    "category": "primary",
+    "url": "http://openblocgrenoble.fr/registration.php",
+    "price": "12EUR"
+  }
+}
+</script>
+  </body>
+</html>
