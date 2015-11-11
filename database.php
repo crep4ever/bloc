38596c51607ca9525adc7ba818a0946a3ec72dc2
Romain Goffe <romain.goffe@gmail.com>
2015-11-11 18:26:51 +0100

    [sql] update default conf to local db

--- database.php
+++ database.php
@@ -2,11 +2,11 @@
 
 class Database
 {
-  private $host      = DB_HOST;
-  private $user      = DB_USER;
-  private $pass      = DB_PASS;
-  private $dbname    = DB_NAME;
- 
+  private $host      = 'localhost';
+  private $user      = 'root';
+  private $pass      = '';
+  private $dbname    = 'openbloc2015';
+
   private $dbh;
 
   private $stmt;
@@ -14,10 +14,10 @@ class Database
   public function __construct()
   {
     $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname . ';charset=utf8';
-    
+
     $options = array(PDO::ATTR_PERSISTENT       => true,
 		     PDO::ATTR_ERRMODE          => PDO::ERRMODE_EXCEPTION);
-    
+
     try
       {
 	$this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
@@ -44,20 +44,20 @@ class Database
 	case is_int($value):
 	  $type = PDO::PARAM_INT;
 	  break;
-	
+
 	case is_bool($value):
 	  $type = PDO::PARAM_BOOL;
 	  break;
-	
+
 	case is_null($value):
 	  $type = PDO::PARAM_NULL;
 	  break;
-	
+
 	default:
 	  $type = PDO::PARAM_STR;
 	}
       }
-    
+
     $this->stmt->bindValue($param, $value, $type);
   }
 
