25359700175052e31691fd7682ebf7385b15efaf
Romain Goffe <romain.goffe@gmail.com>
2015-11-14 22:17:09 +0100

    [content] accordion style for benevoles page

--- benevoles.php
+++ benevoles.php
@@ -1,5 +1,15 @@
 <?php include("header.php"); ?>
 
+<script>
+$(function() {
+  $( "#accordion" ).accordion({
+    collapsible: true,
+    active: false,
+    heightStyle: "content"
+  });
+});
+</script>
+
 <section id="main" class="wrapper style1">
   <header class="major">
     <h2>Cher bénévole : c'est grâce à toi que cet évènement est possible !</h2>
@@ -11,85 +21,64 @@
 
   <div class="container">
 
-    <p style="text-align: center; font-size: 120%;">
-
-      <br /> <b class="red"> MISE A JOUR 02/06 : Organisation et Planning Bénévoles : <a href="images/planning_benevoles.pdf" target="_blank"> cliquer ici </a> </b>
-      <br /> <br />    Merci de <a href="https://docs.google.com/forms/d/1DmxhyqLcMlYdcEAbtX2zekUH-KBXIdff4Vj161JdKEA/viewform" target="_blank">vous positionner à l'aide de ce formulaire</a> sur l'une des missions ci-dessous&nbsp;,
-      selon vos préférences et/ou les besoins résumés en temps réel <a href="https://docs.google.com/spreadsheets/d/1LcM0C_mSpIOakY4dHhFNgaip1kade2DrMJBzkFlS3zA/edit#gid=127100981" target="_blank"> dans ce fichier. </a>
-    </p>
-
-    <div class="row uniform">
-      <div class="4u 6u(2) 12u$(3)">
-        <section class="feature fa-tag">
-          <h3>Installation de la salle</h3>
-          <div class="icon fa-clock-o">&nbsp;la veille au soir&nbsp;</div>
-          <div class="icon fa-users">&nbsp;15&nbsp;</div>
-          <div class="icon fa-star">&nbsp;&nbsp;</div>
-          <p>Positionner les chaises, les barrières, les tapis, la sono ...</p>
-        </section>
+    <div id="accordion">
+      <h3>Installation de la salle</h3>
+      <div>
+        <div class="icon fa-clock-o">&nbsp;la veille au soir&nbsp;</div>
+        <div class="icon fa-users">&nbsp;15&nbsp;</div>
+        <div class="icon fa-star">&nbsp;&nbsp;</div>
+        <p>Positionner les chaises, les barrières, les tapis, la sono ...</p>
       </div>
 
-      <div class="4u 6u$(2) 12u$(3)">
-        <section class="feature fa-tag">
-          <h3>Buvette</h3>
-          <div class="icon fa-clock-o">&nbsp;permanence toute la journée, rush entre 11h30 et 13h30&nbsp;</div>
-          <div class="icon fa-users">&nbsp;5 ; compatible pointage&nbsp;</div>
-          <div class="icon fa-star">&nbsp;Qui veut être le roi du hot-dog&nbsp;?&nbsp;</div>
-          <p>Tenir le stand buvette, confectionner les sandwiches, les crêpes ...</p>
-        </section>
+      <h3>Buvette</h3>
+      <div>
+        <div class="icon fa-clock-o">&nbsp;permanence toute la journée, rush entre 11h30 et 13h30&nbsp;</div>
+        <div class="icon fa-users">&nbsp;5 ; compatible pointage&nbsp;</div>
+        <div class="icon fa-star">&nbsp;Qui veut être le roi du hot-dog&nbsp;?&nbsp;</div>
+        <p>Tenir le stand buvette, confectionner les sandwiches, les crêpes ...</p>
       </div>
 
-      <div class="4u$ 6u(2) 12u$(3)">
-        <section class="feature fa-tag">
-          <h3>Pointage des compétiteurs</h3>
-          <div class="icon fa-clock-o">&nbsp;8h30-9h15 puis 11h-12h&nbsp;</div>
-          <div class="icon fa-users">&nbsp;5 ; compatible buvette&nbsp;</div>
-          <div class="icon fa-star">&nbsp;&nbsp;</div>
-          <p>Vérifier les licences, remettre les dossards ...</p>
-        </section>
+      <h3>Pointage des compétiteurs</h3>
+      <div>
+        <div class="icon fa-clock-o">&nbsp;8h30-9h15 puis 11h-12h&nbsp;</div>
+        <div class="icon fa-users">&nbsp;5 ; compatible buvette&nbsp;</div>
+        <div class="icon fa-star">&nbsp;&nbsp;</div>
+        <p>Vérifier les licences, remettre les dossards ...</p>
       </div>
 
-      <div class="4u 6u$(2) 12u$(3)">
-        <section class="feature fa-tag">
-          <h3>Juges de blocs</h3>
-          <div class="icon fa-clock-o">&nbsp;9h30-12 et/ou 12h30-15h&nbsp;</div>
-          <div class="icon fa-users">&nbsp;30 (+formation préalable)&nbsp;</div>
-          <div class="icon fa-star">&nbsp;Fabien&nbsp;</div>
-          <p>Organiser le passage des jeunes dans le bloc dont vous êtes responsable, observer et inscrire leur prestation sur les feuilles de route.</p>
-        </section>
+      <h3>Juges de blocs</h3>
+      <div>
+        <div class="icon fa-clock-o">&nbsp;9h30-12 et/ou 12h30-15h&nbsp;</div>
+        <div class="icon fa-users">&nbsp;30 (+formation préalable)&nbsp;</div>
+        <div class="icon fa-star">&nbsp;Fabien&nbsp;</div>
+        <p>Organiser le passage des jeunes dans le bloc dont vous êtes responsable, observer et inscrire leur prestation sur les feuilles de route.</p>
       </div>
 
-      <div class="4u 6u(2) 12u$(3)">
-        <section class="feature fa-tag">
-          <h3>Saisie des résultats</h3>
-          <div class="icon fa-clock-o">&nbsp;12h-13h30 puis 15h-16h30&nbsp;</div>
-          <div class="icon fa-users">&nbsp;4 (+formation préalable)&nbsp;</div>
-          <div class="icon fa-star">&nbsp;Romain/Viviane &nbsp;</div>
-          <p>Saisir informatiquement les résultats, en binôme. Concentration et rigueur sont de mise !</p>
-        </section>
+      <h3>Saisie des résultats</h3>
+      <div>
+        <div class="icon fa-clock-o">&nbsp;12h-13h30 puis 15h-16h30&nbsp;</div>
+        <div class="icon fa-users">&nbsp;4 (+formation préalable)&nbsp;</div>
+        <div class="icon fa-star">&nbsp;Romain/Viviane &nbsp;</div>
+        <p>Saisir informatiquement les résultats, en binôme. Concentration et rigueur sont de mise !</p>
       </div>
 
-      <div class="4u 6u(2) 12u$(3)">
-        <section class="feature fa-tag">
-          <h3>Photos &amp; Vidéos</h3>
-          <div class="icon fa-clock-o">&nbsp;toute la journée est à couvrir&nbsp;</div>
-          <div class="icon fa-users">&nbsp;mini 1 photo + 1 vidéo &nbsp;</div>
-          <div class="icon fa-star">&nbsp;Grégoire&nbsp;</div>
-          <p>Couvrir l'évènement. Les photographes "officiels" pourront avoir accès aux zones propices à la prise de vue.</p>
-        </section>
+      <h3>Photos &amp; Vidéos</h3>
+      <div>
+        <div class="icon fa-clock-o">&nbsp;toute la journée est à couvrir&nbsp;</div>
+        <div class="icon fa-users">&nbsp;mini 1 photo + 1 vidéo &nbsp;</div>
+        <div class="icon fa-star">&nbsp;Grégoire&nbsp;</div>
+        <p>Couvrir l'évènement. Les photographes "officiels" pourront avoir accès aux zones propices à la prise de vue.</p>
       </div>
 
-      <div class="4u 6u(2) 12u$(3)">
-        <section class="feature fa-tag">
-          <h3>Rangement de la salle</h3>
-          <div class="icon fa-clock-o">&nbsp;À partir de 16h30&nbsp;</div>
-          <div class="icon fa-users">&nbsp;15&nbsp;</div>
-          <div class="icon fa-star">&nbsp;&nbsp;</div>
-          <p>... et après on boit un coup ? ;-)</p>
-        </section>
+      <h3>Rangement de la salle</h3>
+      <div>
+        <div class="icon fa-clock-o">&nbsp;À partir de 16h30&nbsp;</div>
+        <div class="icon fa-users">&nbsp;15&nbsp;</div>
+        <div class="icon fa-star">&nbsp;&nbsp;</div>
+        <p>... et après on boit un coup ? ;-)</p>
       </div>
-
     </div>
+
   </div>
 </section>
 
