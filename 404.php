73802d21ba89e437823955b7b4ed0865a78426b8
Romain Goffe <romain.goffe@gmail.com>
2015-12-31 12:33:37 +0100

    [php] rework 404

--- 404.php
+++ 404.php
@@ -1,24 +1,28 @@
-<?php include("header.php"); ?>
+<!DOCTYPE HTML>
+<html lang="fr">
 
-<section id="main" class="wrapper style1">
+<head>
+  <title>Open de bloc 2016</title>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="description" content="Open de bloc de Grenoble 2016" />
+  <meta name="keywords" content="escalade, open, bloc, compétition, grenoble, drac vercors escalade" />
+  <link rel="stylesheet" href="/css/bloc.css" />
+</head>
 
-  <div class="container">
+<body>
+  <p>
+    <span class="image left"><img src="/images/fabien.jpg" alt="404" /></span>
+  </p>
 
-    <p>
-      <span class="image left"><img src="/images/fabien.jpg" alt="404" /></span>
-    </p>
+  <p style="padding:  4em 0em 0em 0em; font-size: 160%; text-align:center; color:#5c3566;">
+    Pour Fabien, c'est pas gagné, mais vous c'est sûr : vous la
+    tenez, votre erreur 404 !
+  </p>
 
-    <p style="padding:  4em 0em 4em 0em; font-size: 160%; text-align:center; color:#5c3566;">
-      Pour Fabien, c'est pas gagné, mais vous c'est sûr : vous la
-      tenez, votre erreur 404 !
-    </p>
+  <p style="padding: 0em 0em 4em 0em; font-size: 140%; text-align:center; color:#5c3566;">
+    Vous pouvez refaire un essai,
+    mais si vous ne trouvez pas la méthode, <a href="www.openblocgrenoble.fr">reposez-vous ici</a>.
+  </p>
+</body>
 
-    <p style="padding: 0em 0em 6em 0em; font-size: 140%; text-align:center; color:#5c3566;">
-      Vous pouvez refaire un essai,
-      mais si vous ne trouvez pas la méthode, <a href="index.php">reposez-vous ici</a>.
-    </p>
-
-  </div>
-</section>
-
-<?php include("footer.php"); ?>
+</html>
