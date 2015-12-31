f9fe79d7b495fdece04257814a8ea182c7da8f16
Romain Goffe <romain.goffe@gmail.com>
2015-12-31 11:12:44 +0100

    [php] rework benevoles page (wip)

---
+++ benevoles-form.inc.php
@@ -0,0 +1,79 @@
+<h2>Se positionner</h2>
+<section id="content">
+
+  <form action="benevole-confirm.php" method="post">
+
+    <fieldset>
+
+      <div class="left">
+        <label for="lastname">Nom</label>
+        <input type="text" name="nom" id="lastname" required>
+      </div>
+
+      <div class="left">
+        <label for="firstname">Prénom</label>
+        <input type="text" name="prenom" id="firstname" required>
+      </div>
+
+      <div class="left">
+        <label for="firstname">Mail</label>
+        <input type="email" id="email" name="email" placeholder="name@provider.com" required>
+      </div>
+
+      <div class="left">
+        <label for="tel">Téléphone</label>
+        <input type="tel" id="tel" name="telephone" placeholder="06XXXXXXXX" maxlength="10" pattern="[0][0-9]{9}" required>
+      </div>
+
+      <div class="clear">&nbsp;</div>
+
+      <div class="left">
+        <label>Missions souhaitées</label>
+        <input type="checkbox" name="missions" id="installation" value="installation">
+        <label for="installation">Installation de la salle</label>
+
+        <input type="checkbox" name="missions" id="buvette" value="buvette">
+        <label for="buvette">Buvette</label>
+
+        <input type="checkbox" name="missions" id="pointage" value="pointage">
+        <label for="pointage">Pointage</label>
+
+        <input type="checkbox" name="missions" id="juge" value="juge">
+        <label for="juge">Juge de bloc</label>
+
+        <input type="checkbox" name="missions" id="saisie" value="saisie">
+        <label for="saisie">Saisie des résultats</label>
+
+        <input type="checkbox" name="missions" id="reportage" value="reportage">
+        <label for="reportage">Reportage photo / video</label>
+
+        <input type="checkbox" name="missions" id="rangement" value="rangement">
+        <label for="rangement">Rangement de la salle</label>
+      </div>
+
+      <div class="clear">&nbsp;</div>
+
+      <div class="left">
+        <label for="dispo">Disponibilité</label>
+        <input type="radio" name="dispo" id="all" value="all">
+        <label for="all">Toute la durée de la mission</label>
+
+        <input type="radio" name="dispo" id="matin" value="matin">
+        <label for="matin">Matin</label>
+
+        <input type="radio" name="dispo" id="aprem" value="aprem">
+        <label for="aprem">Après-midi</label>
+      </div>
+
+      <div class="clear">&nbsp;</div>
+
+      <textarea name="message" id="message" placeholder="Un commentaire ?" rows="4"></textarea>
+
+      <div class="clear">&nbsp;</div>
+
+      <button type="submit" class="button big" role="button" aria-disabled="false">Valider</button>
+
+    </fieldset>
+  </form>
+
+</section>
---
+++ benevoles-missions.inc.php
@@ -0,0 +1,69 @@
+<script>
+$(function() {
+  $( "#accordion" ).accordion({
+    collapsible: true,
+    active: false,
+    heightStyle: "content"
+  });
+});
+</script>
+
+<h2>Description des missions</h2>
+
+<div id="accordion">
+  <h3>Installation de la salle<span class="effectif">1 / 3</span></h3>
+  <div>
+    <div class="icon fa-clock-o">&nbsp;la veille au soir&nbsp;</div>
+    <div class="icon fa-users">&nbsp;15&nbsp;</div>
+    <div class="icon fa-star">&nbsp;&nbsp;</div>
+    <p>Positionner les chaises, les barrières, les tapis, la sono ...</p>
+  </div>
+
+  <h3>Buvette</h3>
+  <div>
+    <div class="icon fa-clock-o">&nbsp;permanence toute la journée, rush entre 11h30 et 13h30&nbsp;</div>
+    <div class="icon fa-users">&nbsp;5 ; compatible pointage&nbsp;</div>
+    <div class="icon fa-star">&nbsp;Qui veut être le roi du hot-dog&nbsp;?&nbsp;</div>
+    <p>Tenir le stand buvette, confectionner les sandwiches, les crêpes ...</p>
+  </div>
+
+  <h3>Pointage des compétiteurs</h3>
+  <div>
+    <div class="icon fa-clock-o">&nbsp;8h30-9h15 puis 11h-12h&nbsp;</div>
+    <div class="icon fa-users">&nbsp;5 ; compatible buvette&nbsp;</div>
+    <div class="icon fa-star">&nbsp;&nbsp;</div>
+    <p>Vérifier les licences, remettre les dossards ...</p>
+  </div>
+
+  <h3>Juges de blocs</h3>
+  <div>
+    <div class="icon fa-clock-o">&nbsp;9h30-12 et/ou 12h30-15h&nbsp;</div>
+    <div class="icon fa-users">&nbsp;30 (+formation préalable)&nbsp;</div>
+    <div class="icon fa-star">&nbsp;Fabien&nbsp;</div>
+    <p>Organiser le passage des jeunes dans le bloc dont vous êtes responsable, observer et inscrire leur prestation sur les feuilles de route.</p>
+  </div>
+
+  <h3>Saisie des résultats</h3>
+  <div>
+    <div class="icon fa-clock-o">&nbsp;12h-13h30 puis 15h-16h30&nbsp;</div>
+    <div class="icon fa-users">&nbsp;4 (+formation préalable)&nbsp;</div>
+    <div class="icon fa-star">&nbsp;Romain/Viviane &nbsp;</div>
+    <p>Saisir informatiquement les résultats, en binôme. Concentration et rigueur sont de mise !</p>
+  </div>
+
+  <h3>Photos &amp; Vidéos</h3>
+  <div>
+    <div class="icon fa-clock-o">&nbsp;toute la journée est à couvrir&nbsp;</div>
+    <div class="icon fa-users">&nbsp;mini 1 photo + 1 vidéo &nbsp;</div>
+    <div class="icon fa-star">&nbsp;Grégoire&nbsp;</div>
+    <p>Couvrir l'évènement. Les photographes "officiels" pourront avoir accès aux zones propices à la prise de vue.</p>
+  </div>
+
+  <h3>Rangement de la salle</h3>
+  <div>
+    <div class="icon fa-clock-o">&nbsp;À partir de 16h30&nbsp;</div>
+    <div class="icon fa-users">&nbsp;15&nbsp;</div>
+    <div class="icon fa-star">&nbsp;&nbsp;</div>
+    <p>... et après on boit un coup ? ;-)</p>
+  </div>
+</div>
