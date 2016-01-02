9a8242106668ed4e1c7f2eca067b45ecb1363f1d
Romain Goffe <romain.goffe@gmail.com>
2016-01-02 22:39:54 +0100

    [php] rework registration info

--- registration-info.inc.php
+++ registration-info.inc.php
@@ -1,24 +1,6 @@
 <?php
 require_once 'globals.php';
 require_once 'database.php';
-?>
-
-<?php if (strtotime('now') < $GLOBALS['registration-open-date']) { ?>
-  <section class="feature feature fa-info-circle">
-    <h3>Les inscriptions ne sont pas encore ouvertes</h3>
-    <p>
-      Merci de revenir plus tard.
-    </p>
-  </section>
-<?php } ?>
-
-<?php if (strtotime('now') > $GLOBALS['registration-close-date']) { ?>
-  <section class="feature feature fa-info-circle">
-    <h3>Les inscriptions sont maintenant fermées</h3>
-  </section>
-<?php } ?>
-
-<?php
 
 // Check available places from already registered candidates in db
 $db->query("SELECT * FROM bloc_2016 WHERE (categorie = :cat1 OR categorie = :cat2) AND payer_id IS NOT NULL");
@@ -32,31 +14,48 @@ $db->bind(':cat1', 'minime');
 $db->bind(':cat2', 'cadet');
 $db->resultset();
 $GLOBALS['remaining-places-minime-cadet'] = max(0, $GLOBALS['available-places'] - $db->rowCount());
-?>
 
-<?php if ($GLOBALS['remaining-places-poussin-benjamin'] == 0) { ?>
-  <section class="feature fa-exclamation-triangle">
-    <h3>Attention</h3>
-    <p>
-      Les inscriptions sont closes pour les catégories <b>Poussin et Benjamin</b>.<br />
-      Vous pouvez néanmoins <a href="contact.php">nous contacter</a> pour une
-      inscription sur liste d'attente.
-    </p>
-  </section>
-<?php } ?>
+$infos = [];
 
-<?php if ($GLOBALS['remaining-places-minime-cadet'] == 0) { ?>
-  <section class="feature fa-exclamation-triangle">
-    <h3>Attention</h3>
-    <p>
-      Les inscriptions sont closes pour les catégories <b>Minime et Cadet</b>.<br />
-      Vous pouvez néanmoins <a href="contact.php">nous contacter</a> pour une
-      inscription sur liste d'attente.
-    </p>
-  </section>
-<?php } ?>
+if (strtotime('now') < $GLOBALS['registration-open-date'])
+{
+  array_push($infos, "Les inscriptions ne sont pas encore ouvertes.");
+}
 
-<?php
+if (strtotime('now') > $GLOBALS['registration-close-date'])
+{
+  array_push($infos, "Les inscriptions sont maintenant fermées.");
+}
+
+if ($GLOBALS['remaining-places-poussin-benjamin'] == 0)
+{
+  array_push($infos, "Les inscriptions sont closes pour les catégories <b>Poussin et Benjamin</b>.<br />
+                      Vous pouvez néanmoins <a href=\"contact.php\">nous contacter</a> pour une
+                      inscription sur liste d'attente.");
+}
+
+if ($GLOBALS['remaining-places-minime-cadet'] == 0)
+{
+  array_push($infos, "Les inscriptions sont closes pour les catégories <b>Minime et Cadet</b>.<br />
+                      Vous pouvez néanmoins <a href=\"contact.php\">nous contacter</a> pour une
+                      inscription sur liste d'attente.");
+}
+
+if (!empty($infos))
+{
+  $infos_html = "<section class=\"feature feature fa-info-circle\">";
+  $infos_html .= "<ul>";
+
+  foreach ($infos as $info)
+  {
+    $infos_html .= "<li>" . $info . "</li>";
+  }
+
+  $infos_html .= "</ul></section>";
+
+  echo $infos_html;
+}
+
 $displayForm = (strtotime('now') >= $GLOBALS['registration-open-date']) &&
 (strtotime('now') <= $GLOBALS['registration-close-date']);
 ?>
