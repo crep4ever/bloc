9fc9362338f322710f9f35b18c5ac030afc2b6fa
Romain Goffe <romain.goffe@gmail.com>
2016-01-02 17:11:51 +0100

    [sql] fix binding

--- candidate-process.php
+++ candidate-process.php
@@ -10,17 +10,17 @@ $db->beginTransaction();
 
 $db->query("INSERT INTO bloc_2016(nom, prenom, sexe, naissance, categorie, licence_type, licence_number, club, experience, comment, session) VALUES(:nom, :prenom, :sexe, :naissance, :categorie, :licence_type, :licence_number, :club, :experience, :comment, :session)");
 
-$db->bind('nom', $_SESSION['lastname']);
-$db->bind('prenom' , $_SESSION['firstname']);
-$db->bind('sexe', $_SESSION['sex']);
-$db->bind('naissance', $_SESSION['birthday_str']);
-$db->bind('categorie', $_SESSION['category']);
-$db->bind('licence_type', $_SESSION['licenceType']);
-$db->bind('licence_number', $_SESSION['licenceNumber']);
-$db->bind('club', $_SESSION['club']);
-$db->bind('experience', $_SESSION['experience']);
-$db->bind('comment', $_SESSION['comment']);
-$db->bind('session', $_SESSION['session']);
+$db->bind(':nom', $_SESSION['lastname']);
+$db->bind(':prenom' , $_SESSION['firstname']);
+$db->bind(':sexe', $_SESSION['sex']);
+$db->bind(':naissance', $_SESSION['birthday_str']);
+$db->bind(':categorie', $_SESSION['category']);
+$db->bind(':licence_type', $_SESSION['licenceType']);
+$db->bind(':licence_number', $_SESSION['licenceNumber']);
+$db->bind(':club', $_SESSION['club']);
+$db->bind(':experience', $_SESSION['experience']);
+$db->bind(':comment', $_SESSION['comment']);
+$db->bind(':session', $_SESSION['session']);
 
 $db->execute();
 $db->endTransaction();
