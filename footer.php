60201156d324b0d70a7071ce33d14f1dafcd8cec
Romain Goffe <romain.goffe@gmail.com>
2016-01-03 02:27:22 +0100

    [css] footer logos as css spritesheet

--- footer.php
+++ footer.php
@@ -1,21 +1,21 @@
 <!-- Footer -->
 <footer id="footer">
   <ul class="icons">
-    <li><a href="http://www.ffme.fr" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/ffme.png" title="FFME" alt="FFME" /></a></li>
-    <li><a href="http://www.ffcam.fr" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/ffcam.png" title="FFCAM" alt="FFCAM" /></a></li>
-    <li><a href="http://ville-fontaine.fr" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/fontaine.png" title="Ville de Fontaine" alt="Ville de Fontaine" /></a></li>
-    <li><a href="http://www.auvieuxcampeur.fr" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/vieux-campeur.jpg" title="Au vieux campeur" alt="Au vieux campeur" /></a></li>
-    <li><a href="http://www.adidas.fr" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/adidas.png" title="Adidas" alt="Adidas" /></a></li>
-    <li><a href="http://www.beal-planet.com/" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/beal.png" title="Beal" alt="Beal" /></a></li>
-    <li><a href="http://www.jegrimpe.com/" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/jegrimpe.png" title="JeGrimpe.com" alt="JeGrimpe.com" /></a></li>
-    <li><a href="http://www.lechaussemontagne.fr/7-chaussons-d-escalade" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/canin.png" title="Canin" alt="Canin" /></a></li>
-    <li><a href="http://www.andrea-boldrini.com/" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/andrea-boldrini.png" title="André Boldrini" alt="Andréa Boldrini" /></a></li>
-    <li><a href="http://www.expe.fr/fr" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/expe.png" title="Expé" alt="Expé" /></a></li>
-    <li><a href="http://www.grimper.com/" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/grimper.png" title="Grimper" alt="Grimper" /></a></li>
-    <li><a href="http://entre-prises.fr/" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/entre-prises.png" title="Entre-Prises" alt="Entre-Prises" /></a></li>
-    <li><a href="http://www.atelierrocabilo.com/" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/atelier-rocabilo.png" title="Atelier Rocabilo" alt="Atelier Rocabilo" /></a></li>
-    <li><a href="http://www.prises-escalade-osmose.com/" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/osmose.png" title="Osmose" alt="Osmose" /></a></li>
-    <li><a href="http://www.ledauphine.com/" target="_blank" rel="nofollow"><img src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" data-src="images/logos/DL.png" title="Dauphiné Libéré" alt="Dauphiné Libéré" /></a></li>
+    <li><a href="http://www.ffme.fr" target="_blank" rel="nofollow"><i class="sprite sprite-ffme"></i></a></li>
+    <li><a href="http://www.ffcam.fr" target="_blank" rel="nofollow"><i class="sprite sprite-ffcam"></i></a></li>
+    <li><a href="http://ville-fontaine.fr" target="_blank" rel="nofollow"><i class="sprite sprite-fontaine"></i></a></li>
+    <li><a href="http://www.auvieuxcampeur.fr" target="_blank" rel="nofollow"><i class="sprite sprite-vieux-campeur"></i></a></li>
+    <li><a href="http://www.adidas.fr" target="_blank" rel="nofollow"><i class="sprite sprite-adidas"></i></a></li>
+    <li><a href="http://www.beal-planet.com/" target="_blank" rel="nofollow"><i class="sprite sprite-beal"></i></a></li>
+    <li><a href="http://www.jegrimpe.com/" target="_blank" rel="nofollow"><i class="sprite sprite-jegrimpe"></i></a></li>
+    <li><a href="http://www.lechaussemontagne.fr/7-chaussons-d-escalade" target="_blank" rel="nofollow"><i class="sprite sprite-canin"></i></a></li>
+    <li><a href="http://www.andrea-boldrini.com/" target="_blank" rel="nofollow"><i class="sprite sprite-andrea-boldrini"></i></a></li>
+    <li><a href="http://www.expe.fr/fr" target="_blank" rel="nofollow"><i class="sprite sprite-expe"></i></a></li>
+    <li><a href="http://www.grimper.com/" target="_blank" rel="nofollow"><i class="sprite sprite-grimper"></i></a></li>
+    <li><a href="http://entre-prises.fr/" target="_blank" rel="nofollow"><i class="sprite sprite-entre-prises"></i></a></li>
+    <li><a href="http://www.atelierrocabilo.com/" target="_blank" rel="nofollow"><i class="sprite sprite-atelier-rocabilo"></i></a></li>
+    <li><a href="http://www.prises-escalade-osmose.com/" target="_blank" rel="nofollow"><i class="sprite sprite-osmose"></i></a></li>
+    <li><a href="http://www.ledauphine.com/" target="_blank" rel="nofollow"><i class="sprite sprite-DL"></i></a></li>
   </ul>
 
   <span class="copyright">
