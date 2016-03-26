cebb6f99ebba0f1e0246dcc29ff01694f5a34455
Romain Goffe <romain.goffe@gmail.com>
2016-03-26 11:42:21 +0100

    [meta] update event markup

--- event-markup.php
+++ event-markup.php
@@ -2,26 +2,26 @@
 {
   "@context": "http://schema.org/",
   "@type": "SportsEvent",
-  "name": "Open de bloc Grenoble 2015",
-  "image": "http://openblocgrenoble.fr/images/open-bloc-2015.jpg",
+  "name": "Open de bloc Grenoble 2016",
+  "image": "http://www.openblocgrenoble.fr/images/open-bloc-2016.jpg",
   "organizer": {
     "name": "Drac Vercors Escalade",
-    "image": "http://openblocgrenoble.fr/images/logos/dve.png",
+    "image": "http://www.openblocgrenoble.fr/images/logos/dve.png",
     "url": "http://fontaine-en-montagne.ffcam.fr/"
   },
-  "typicalAgeRange": "10-17",
-  "description": "3ème édition de l'Open de bloc de Grenoble",
-  "startDate": "2015-06-07T08:30-0400",
+  "typicalAgeRange": "8-15",
+  "description": "4ème édition de l'Open de bloc de Grenoble",
+  "startDate": "2016-06-05T08:30-0400",
   "location": {
     "@type": "Place",
     "name": "Espace vertical 3",
     "address": "22 rue Victor Lastella, 38000 Grenoble, France",
-    "sameAs": "http://openblocgrenoble.fr/access.php"
+    "sameAs": "http://www.openblocgrenoble.fr/access.php"
   },
   "offers": {
     "@type": "Offer",
     "category": "primary",
-    "url": "http://openblocgrenoble.fr/registration.php",
+    "url": "http://www.openblocgrenoble.fr/registration.php",
     "price": "12EUR"
   }
 }
