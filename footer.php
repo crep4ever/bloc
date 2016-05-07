b13de75c5fd13a5cd33c1242db3853fb4f50ab6d
Romain Goffe <romain.goffe@gmail.com>
2016-05-07 13:24:00 +0200

    [css] update logos

--- footer.php
+++ footer.php
@@ -4,11 +4,20 @@
     <li><a href="http://ville-fontaine.fr" rel="nofollow"><i class="sprite sprite-fontaine"></i></a></li>
     <li><a href="http://www.ffme.fr" rel="nofollow"><i class="sprite sprite-ffme"></i></a></li>
     <li><a href="http://www.ffcam.fr" rel="nofollow"><i class="sprite sprite-ffcam"></i></a></li>
+    <li><a href="http://www.adidas.fr" rel="nofollow"><i class="sprite sprite-adidas"></i></a></li>
     <li><a href="http://www.auvieuxcampeur.fr" rel="nofollow"><i class="sprite sprite-vieux-campeur"></i></a></li>
+    <li><a href="http://www.petzl.com" rel="nofollow"><i class="sprite sprite-petzl"></i></a></li>
+    <li><a href="http://www.expe.fr" rel="nofollow"><i class="sprite sprite-expe"></i></a></li>
+    <li><a href="http://www.scarpa.net" rel="nofollow"><i class="sprite sprite-scarpa"></i></a></li>
+    <li><a href="http://www.andrea-boldrini.com" rel="nofollow"><i class="sprite sprite-andrea-boldrini"></i></a></li>
     <li><a href="http://www.beal-planet.com/" rel="nofollow"><i class="sprite sprite-beal"></i></a></li>
-    <li><a href="http://www.expe.fr/fr" rel="nofollow"><i class="sprite sprite-expe"></i></a></li>
-    <li><a href="http://www.atelierrocabilo.com/" rel="nofollow"><i class="sprite sprite-atelier-rocabilo"></i></a></li>
-    <li><a href="http://www.scarpa.net/en/" rel="nofollow"><i class="sprite sprite-scarpa"></i></a></li>
+    <li><a href="http://www.atelierrocabilo.com" rel="nofollow"><i class="sprite sprite-atelier-rocabilo"></i></a></li>
+    <li><a href="http://www.lechaussemontagne.fr" rel="nofollow"><i class="sprite sprite-canin"></i></a></li>
+    <li><a href="http://www.banque-rhone-alpes.fr" rel="nofollow"><i class="sprite sprite-banque-rhone-alpes"></i></a></li>
+    <li><a href="http://www.entre-prises.fr" rel="nofollow"><i class="sprite sprite-entreprises"></i></a></li>
+    <li><a href="http://eska-vetements.fr" rel="nofollow"><i class="sprite sprite-eska"></i></a></li>
+    <li><a href="http://www.abk-climbing.eu" rel="nofollow"><i class="sprite sprite-abk"></i></a></li>
+    <li><a href="http://www.teisseire.fr" rel="nofollow"><i class="sprite sprite-tesseire"></i></a></li>                            
   </ul>
 
   <span class="copyright">
