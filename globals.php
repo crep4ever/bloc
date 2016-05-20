b5a88e0d59201b02e4af37d38fda0eda976c4dcf
Romain Goffe <romain.goffe@gmail.com>
2016-05-20 07:52:21 +0200

    [php] fix arrival hours of categories in confirmation email

--- globals.php
+++ globals.php
@@ -22,11 +22,11 @@ $day = 31; $month = 5;  $year = 2016;
 $GLOBALS['registration-close-date'] = mktime($hour, $minute, $second, $month, $day, $year);
 $GLOBALS['registration-close-date-str'] = strftime('%A %e %B %G', $GLOBALS['registration-close-date']);
 
-$hour = 11; $minute = 0; $second = 0;
+$hour = 8; $minute = 30; $second = 0;
 $day = 5;   $month = 6;  $year = 2016;
 $GLOBALS['arrival-morning'] = mktime($hour, $minute, $second, $month, $day, $year);
 
-$hour = 8; $minute = 30; $second = 0;
+$hour = 11; $minute = 0; $second = 0;
 $day = 5;  $month = 6;   $year = 2016;
 $GLOBALS['arrival-afternoon'] = mktime($hour, $minute, $second, $month, $day, $year);
 
